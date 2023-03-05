44cce0cd5557e83ce1ebf0c47434930b9066c51d
SkMihail <msklyarov@gmail.com>
2023-03-05 19:24:05 +1000

    5.1.2.Итератор четных чисел [#150]

---
+++ src/main/java/iterator/EvenNumbersIterator.java
@@ -0,0 +1,32 @@
+package iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (point < data.length) {
+            if (data[point] % 2 == 0) {
+                break;
+            }
+            point++;
+        }
+        return point < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point++];
+    }
+}
