2999385bdecd1a386f5a084f492c659a414317d4
SkMihail <msklyarov@gmail.com>
2023-03-08 00:07:32 +1000

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/collection/SimpleArrayList.java
@@ -0,0 +1,84 @@
+package collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+    private T[] container;
+    private int size = 0;
+    private int modCount = 0;
+
+    public SimpleArrayList(int capacity) {
+        if (capacity < 2) {
+            capacity = 2;
+        }
+        container = (T[]) new Object[capacity];
+    }
+
+    private void grow() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            grow();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T temp = container[index];
+        container[index] = newValue;
+        return temp;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T temp = container[index];
+        System.arraycopy(container, index + 1, container, index, size - index - 1);
+        container[size - 1] = null;
+        size--;
+        modCount++;
+        return temp;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int point = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[point++];
+            }
+        };
+    }
+}
+
