af56e4e909f4908569df82cea261dc0a5e721d08
SkMihail <msklyarov@gmail.com>
2023-03-19 16:24:37 +1000

    6. Переопределить только equals [#1004]

--- src/main/java/map/User.java
+++ src/main/java/map/User.java
@@ -6,6 +6,18 @@ import java.util.Map;
 import java.util.Objects;
 
 public class User {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
     private String name;
     private int children;
     private Calendar birthday;
@@ -14,10 +26,6 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
-    }
-
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
     }
 
     public static void main(String[] args) {
