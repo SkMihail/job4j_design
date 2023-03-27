d169f7c2579f4ecc340190c02a60e300dcd65f07
SkMihail <msklyarov@gmail.com>
2023-03-27 14:13:51 +1000

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/tree/SimpleTree.java
+++ src/main/java/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -8,7 +9,6 @@ public class SimpleTree<E> implements Tree<E> {
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
     }
-
 
     @Override
     public boolean add(E parent, E child) {
@@ -22,21 +22,25 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(node -> node.value.equals(value));
+    }
+
+    public boolean isBinary() {
+        return findByPredicate(node -> node.children.size() > 2).isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
         }
         return rsl;
-    }
-
-    public boolean isBinary() {
-        return false;
     }
 }
