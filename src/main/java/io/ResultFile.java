135ad7b7b59b9079fe848c8d1c6140cc3b5e0162
SkMihail <msklyarov@gmail.com>
2023-03-28 22:41:48 +1000

    2.0. FileOutputStream [#252488]

--- src/main/java/io/ResultFile.java
+++ src/main/java/io/ResultFile.java
@@ -1,27 +1,22 @@
 package io;
 
 import java.io.FileOutputStream;
-import java.util.Arrays;
 
 public class ResultFile {
-    public static int[][] multiple(int size) {
-        int[][] array = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                array[i][j] = (i + 1) * (j + 1);
-            }
-        }
-        return array;
-    }
-    public static void main(String[] args) {
-
+    public static void multipleToFile(int size) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            for (int[] array : multiple(5)) {
-                out.write(Arrays.toString(array).getBytes());
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    out.write(Integer.toString((i + 1) * (j + 1)).getBytes());
+                }
                 out.write(System.lineSeparator().getBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        multipleToFile(8);
     }
 }
