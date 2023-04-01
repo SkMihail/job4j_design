a87fc9731853fe040a065be8f34e8c519776e793
SkMihail <msklyarov@gmail.com>
2023-04-01 21:51:11 +1000

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/io/Config.java
@@ -0,0 +1,64 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.StringJoiner;
+import java.util.stream.Stream;
+
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    private static Map<String, String> checkLineForTemplate(String line) {
+        Map<String, String> res = Stream.of(line)
+                .filter(x -> !x.startsWith("#") && !x.isEmpty())
+                .collect(Collectors.toMap(x -> x.substring(0, x.indexOf("=")),
+                        x -> x.substring(x.indexOf("=") + 1)));
+
+        if (res.containsKey("") || res.containsValue("")) {
+            throw new IllegalArgumentException("Line contains an invalid template");
+        }
+        return res;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .forEach(x -> values.putAll(checkLineForTemplate(x)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+        Config configurator = new Config("data/app.properties");
+        configurator.load();
+        System.out.println(configurator.values);
+    }
+}
---
+++ src/test/java/io/ConfigTest.java
@@ -0,0 +1,52 @@
+package io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+    }
+
+    @Test
+    void whenWithComment() {
+        String path = "data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+        assertThat(config.value("#")).isNull();
+    }
+
+    @Test
+    void testWithSeveralSignOfEquals() {
+        String path = "data/withSeveralSignOfEquals.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate")).isEqualTo("connection.password=password=");
+    }
+    @Test
+    void testLoadWithInvalidTemplate() {
+        String path = "data/with_illegal_Template.properties";
+        Config config = new Config(path);
+        Exception exception = assertThrows(IllegalArgumentException.class, config::load);
+        String expectedMessage = "Line contains an invalid template";
+        String actualMessage = exception.getMessage();
+        assertThat(expectedMessage).isEqualTo(actualMessage);
+    }
+
+    @Test
+    void testLoadWhenOneSignOfEqualsAtEnd() {
+        String path = "data/with_illegal_Template2.properties";
+        Config config = new Config(path);
+        Exception exception = assertThrows(IllegalArgumentException.class, config::load);
+        String expectedMessage = "Line contains an invalid template";
+        String actualMessage = exception.getMessage();
+        assertThat(expectedMessage).isEqualTo(actualMessage);
+    }
+}
