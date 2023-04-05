373f843f1462c0df972b6bcc9eabcd12074ad0de
SkMihail <msklyarov@gmail.com>
2023-04-05 16:00:59 +1000

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/io/DuplicatesVisitor.java
@@ -0,0 +1,32 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    HashMap<Path, FileProperty> result = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        result.put(file, new FileProperty(attrs.size(), file.toFile().getName()));
+
+        Map<FileProperty, List<Path>> duplicates = result.entrySet().stream()
+                .collect(Collectors.groupingBy(Map.Entry::getValue,
+                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
+
+        duplicates.entrySet().stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .forEach(entry ->
+                        System.out.println(entry.getKey()
+                                + " повторяется в директориях: " + "\n" +  entry.getValue()));
+        return super.visitFile(file, attrs);
+    }
+}
---
+++ src/main/java/io/FileProperty.java
@@ -0,0 +1,4 @@
+package io;
+
+record FileProperty(long size, String name) {
+}
