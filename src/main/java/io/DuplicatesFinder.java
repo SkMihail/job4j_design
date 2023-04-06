7515f0d5819d455a6ae97f2bf4b47a265458dc86
SkMihail <msklyarov@gmail.com>
2023-04-06 21:08:49 +1000

    11. Поиск дубликатов [#315066] метод поиска дубликтов вернул в DuplicateVisitor

--- src/main/java/io/DuplicatesFinder.java
+++ src/main/java/io/DuplicatesFinder.java
@@ -1,31 +1,10 @@
 package io;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 
 public class DuplicatesFinder {
-    public static void printDup(String dir) throws IOException {
-        Path file = Path.of(dir);
-        DuplicatesVisitor visitor = new DuplicatesVisitor();
-        Files.walkFileTree(file, visitor);
-        Map<FileProperty, List<Path>> duplicates = visitor.getResult().entrySet().stream()
-                .collect(Collectors.groupingBy(Map.Entry::getValue,
-                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
-
-        duplicates.entrySet().stream()
-                .filter(entry -> entry.getValue().size() > 1)
-                .forEach(entry ->
-                        System.out.println(entry.getKey()
-                                + " повторяется в директориях: " + "\n" +  entry.getValue()));
-        System.out.printf("Всего повторяющихся файлов: %d", duplicates.size());
-    }
 
     public static void main(String[] args) throws IOException {
-        printDup("c:\\projects");
+        new DuplicatesVisitor().printDup("c:\\projects");
     }
 }
--- src/main/java/io/DuplicatesVisitor.java
+++ src/main/java/io/DuplicatesVisitor.java
@@ -2,19 +2,32 @@ package io;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     private static Map<Path, FileProperty> result = new HashMap<>();
 
-    public Map<Path, FileProperty> getResult() {
-        return result;
-    }
+    public void printDup(String dir) throws IOException {
+        Path file = Path.of(dir);
+        Files.walkFileTree(file, this);
+        Map<FileProperty, List<Path>> duplicates = result.entrySet().stream()
+                .collect(Collectors.groupingBy(Map.Entry::getValue,
+                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
 
+        duplicates.entrySet().stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .forEach(entry ->
+                        System.out.println(entry.getKey()
+                                + " повторяется в директориях: " + "\n" +  entry.getValue()));
+        System.out.printf("Всего повторяющихся файлов: %d", duplicates.size());
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
