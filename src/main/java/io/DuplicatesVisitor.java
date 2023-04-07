0f2061851f480428178a47a420a07924dd3fd14c
SkMihail <msklyarov@gmail.com>
2023-04-07 11:32:50 +1000

    11. Поиск дубликатов [#315066] изменил структуру Map<FileProperty, List<Path>> result

--- src/main/java/io/DuplicatesVisitor.java
+++ src/main/java/io/DuplicatesVisitor.java
@@ -5,29 +5,32 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private static Map<Path, FileProperty> result = new HashMap<>();
+    private static Map<FileProperty, List<Path>> result = new HashMap<>();
 
     public void printDuplicates() {
-        Map<FileProperty, List<Path>> duplicates = result.entrySet().stream()
-                .collect(Collectors.groupingBy(Map.Entry::getValue,
-                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
-
-        duplicates.entrySet().stream()
-                .filter(entry -> entry.getValue().size() > 1)
-                .forEach(entry ->
+       result.entrySet().stream()
+               .filter(x -> x.getValue().size() > 1)
+               .forEach(entry ->
                         System.out.println(entry.getKey()
                                 + " повторяется в директориях: " + "\n" +  entry.getValue()));
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        result.put(file, new FileProperty(attrs.size(), file.toFile().getName()));
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.toFile().getName());
+        if (result.containsKey(fileProperty)) {
+            result.get(fileProperty).add(file);
+        } else {
+            List<Path> paths = new ArrayList<>();
+            paths.add(file);
+            result.put(fileProperty, paths);
+        }
         return super.visitFile(file, attrs);
     }
 }
