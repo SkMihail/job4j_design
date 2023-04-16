399a380dd459356b20de7c694a52bce16d3b76f5
SkMihail <msklyarov@gmail.com>
2023-04-16 19:38:52 +1000

    3. Бот [#7921] убрал цикл for так как нужна только первая строка дял обработки.

--- src/main/java/io/EchoServer.java
+++ src/main/java/io/EchoServer.java
@@ -13,19 +13,18 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("?msg=")) {
-                            String response = "what";
-                            if (str.contains("?msg=Hello")) {
-                                response = "Hello from server!";
-                            }
-                            if (str.contains("?msg=Exit")) {
-                                response = "Connection is not available more!";
-                                server.close();
-                            }
-                            out.write((response + "\r\n\r\n").getBytes());
+                    String str = in.readLine();
+                    System.out.println(str);
+                    if (str.contains("?msg=")) {
+                        String response = "what";
+                        if (str.contains("?msg=Hello")) {
+                            response = "Hello from server!";
                         }
+                        if (str.contains("?msg=Exit")) {
+                            response = "Connection is not available more!";
+                            server.close();
+                        }
+                        out.write((response + "\r\n\r\n").getBytes());
                     }
                     out.flush();
                 }
