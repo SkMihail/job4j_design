5ec9e2ccd57beae7859733d9c3cd630c097e7995
SkMihail <msklyarov@gmail.com>
2023-04-25 22:55:45 +1000

    0. Что такое база данных. [#6860]

--- db/create.sql
+++ db/create.sql
@@ -1,6 +1,6 @@
 create table sales(
 id serial primary key,
-name text,
+customer text,
 phone bigint,
 date_of_order date,
 value bigint,
@@ -9,8 +9,7 @@ status boolean);
 insert into sales (name, phone, date_of_order, value, status) values ('Ольга', '79025456615', '08.08.2023', '5', 'true');
 insert into sales (name, phone, date_of_order, value, status) values ('Иван', '7908651642', '10.08.2023', '1', 'false');
 delete from sales;
-alter table sales;
-alter column phone TYPE text;
+alter table sales alter column phone type varchar;
 insert into sales (name, phone, date_of_order, value, status) values ('Иван', '7908651642', '10.08.2023', '1', 'false');
 select * from sales;
 
